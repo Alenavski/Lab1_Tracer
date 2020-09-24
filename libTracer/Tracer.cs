0615c742df12f3f2c8dbd2b67e9b7934bcc168ec
Alenavski <boom.alena.qwerty@gmail.com>
2020-09-24 13:29:55 +0300

    initial commit

---
+++ libTracer/Tracer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace libTracer
+{
+    public interface ITracer
+    {
+        void StartTrace();
+        void StopTrace();
+        TraceResult GetTraceResult();
+    }
+
+    public class TraceResult
+    {
+        public List<ThreadInfo> Threads { get; private set; }
+    }
+
+    public class ThreadInfo
+    {
+        public int Id_thread { get; private set; }
+        public double Time { get; private set; }
+        public List<MethodInfo> Method { get; private set; }
+    }
+
+    public class MethodInfo
+    {
+        public string Name { get; private set; }
+        public string Class { get; private set; }
+        public double Time { get; private set; }
+        public List<MethodInfo> ChildMethods { get; private set; }
+
+    }
+    public class Tracer : ITracer
+    {
+        
+        public void StartTrace()
+        {
+            
+        }
+        public void StopTrace()
+        {
+          
+        }
+        public TraceResult GetTraceResult()
+        {
+            return new TraceResult();
+        }
+    }
+    
+}
