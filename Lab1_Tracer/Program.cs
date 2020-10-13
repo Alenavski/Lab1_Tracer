6637bbfea6dbb14d266dc5ae6c0eb55e28a6b272
Alenavski <boom.alena.qwerty@gmail.com>
2020-10-13 11:14:41 +0300

    (StartTrace, StopTrace, TraceResult) + main for debug

--- Lab1_Tracer/Program.cs
+++ Lab1_Tracer/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using libTracer;
 
 namespace Lab1_Tracer
 {
@@ -10,7 +12,74 @@ namespace Lab1_Tracer
     {
         static void Main(string[] args)
         {
+            Tracer tr = new Tracer();
+            tr.StartTrace();
+
+            Foo f = new Foo(tr);
+            f.MyMethod();
+
+            SomeClass sc = new SomeClass(tr);
+            Thread th2 = new Thread(new ThreadStart(sc.NewMethod));
+            th2.Start();
+            th2.Join();
+
+            tr.StopTrace();
+            TraceResult trR = tr.GetTraceResult();
+            
             Console.ReadLine();
+        }
+    }
+
+    public class Foo
+    {
+        private Bar _bar;
+        private ITracer _tracer;
+
+        internal Foo(ITracer tracer)
+        {
+            _tracer = tracer;
+            _bar = new Bar(_tracer);
+        }
+        public void MyMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(50);
+            _bar.InnerMethod();
+            _tracer.StopTrace();
+        }
+    }
+
+    public class Bar
+    {
+        private ITracer _tracer;
+
+        internal Bar(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void InnerMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(50);
+            _tracer.StopTrace();
+        }
+    }
+
+    public class SomeClass
+    {
+        private ITracer _tracer;
+
+        internal SomeClass(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void NewMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(100);
+            _tracer.StopTrace();
         }
     }
 }
--- libTracer/Tracer.cs
+++ libTracer/Tracer.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Threading;
 
 namespace libTracer
@@ -17,38 +15,90 @@ namespace libTracer
 
     public class TraceResult
     {
-        public List<ThreadInfo> Threads { get; private set; }
+        public List<ThreadInfo> Threads { get; internal set; }        
     }
 
     public class ThreadInfo
     {
-        public int Id_thread { get; private set; }
-        public double Time { get; private set; }
-        public List<MethodInfo> Method { get; private set; }
+        public int IdThread { get; internal set; }
+        public double Time { get; internal set; }
+        public List<MethodInfo> Method { get; internal set; }
     }
 
     public class MethodInfo
     {
-        public string Name { get; private set; }
-        public string Class { get; private set; }
-        public double Time { get; private set; }
-        public List<MethodInfo> ChildMethods { get; private set; }
+        public string Name { get; internal set; }
+        public string Class { get; internal set; }
+        public double Time { get; internal set; }
+        public List<MethodInfo> ChildMethods { get; internal set; }
 
     }
     public class Tracer : ITracer
     {
-        
+        readonly TraceResult _trace = new TraceResult();        
+        readonly ConcurrentDictionary<int, Stack<(Stopwatch,MethodInfo)>> _threadInfos = new ConcurrentDictionary<int, Stack<(Stopwatch,MethodInfo)>>();
+
         public void StartTrace()
         {
-            
+            Stopwatch sw = new Stopwatch();
+            MethodInfo methodInfo = new MethodInfo();
+
+            sw.Start();
+            int id = Thread.CurrentThread.ManagedThreadId;
+
+            if (_trace.Threads == null)
+            {
+                _trace.Threads = new List<ThreadInfo>();
+            }
+
+            if (_threadInfos.TryAdd(id, new Stack<(Stopwatch, MethodInfo)>()))
+            {
+                _trace.Threads.Add(new ThreadInfo() {IdThread = id});
+            }
+
+            StackFrame sf = new StackFrame(1);
+            MethodBase mb = sf.GetMethod();
+            methodInfo.Name = mb.Name;
+            methodInfo.Class = !(mb.DeclaringType is null) ? mb.DeclaringType.Name : "Unknown";
+            _threadInfos[id].Push((sw,methodInfo));
+
         }
+
         public void StopTrace()
         {
-          
+            int id = Thread.CurrentThread.ManagedThreadId;
+            Stopwatch sw;
+            MethodInfo methodInfo;
+            (sw, methodInfo) = _threadInfos[id].Pop();
+            methodInfo.Time = sw.ElapsedMilliseconds;
+            sw.Stop();
+            if (_threadInfos[id].Count > 0)
+            {
+                MethodInfo mi;
+                (_, mi) = _threadInfos[id].Peek();
+                if (mi.ChildMethods == null)
+                {
+                    mi.ChildMethods = new List<MethodInfo>();
+                }
+
+                mi.ChildMethods.Add(methodInfo);
+            }
+            else
+            {
+                int index = _trace.Threads.FindIndex(thread => thread.IdThread == id);
+                if (_trace.Threads[index].Method == null)
+                {
+                    _trace.Threads[index].Method = new List<MethodInfo>();
+                }
+
+                _trace.Threads[index].Method.Add(methodInfo);
+                _trace.Threads[index].Time += methodInfo.Time;
+            }
         }
+
         public TraceResult GetTraceResult()
         {
-            return new TraceResult();
+            return _trace;
         }
     }
     
